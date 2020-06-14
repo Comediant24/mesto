483c5b3b3addff7bafdac377c1a13995ee4587c6
Iurii Bairamukov <juribai@mail.ru>
2020-06-14 13:43:53 +0700

    fix: исправить пордок занесения данных в форму

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,25 @@
+const editButton = document.querySelector('.profile__user-edit-button');
+const nameUser = document.querySelector('.profile__user-name');
+const jobUser = document.querySelector('.profile__user-job');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
-const editButton = document.querySelector('.profile__user-edit-button');
+const popupOverlay = document.querySelector('.popup__overlay');
 const formPopup = document.querySelector('.popup__edit-form');
 const nameInput = popup.querySelector('.popup__input_type_name');
 const jobInput = popup.querySelector('.popup__input_type_status');
-const nameUser = document.querySelector('.profile__user-name');
-const jobUser = document.querySelector('.profile__user-job');
-const popupOverlay = document.querySelector('.popup__overlay');
 
+// Функция переключатель класса у popup
 function popupToggle () {
-  nameInput.value = nameUser.textContent;
-  jobInput.value = jobUser.textContent;
-  if (popup.classList.contains('popup_opened')) {
+  if (!(popup.classList.contains('popup_opened'))) {  // проверка отсутствия у popup модификатора popup_opened
+    nameInput.value = nameUser.textContent;
+    jobInput.value = jobUser.textContent;
     popup.classList.toggle('popup_opened');
   } else {
     popup.classList.toggle('popup_opened');
   }
 }
 
+// Функция для отправки введенной информации 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameUser.textContent = nameInput.value;
@@ -25,6 +27,7 @@ function formSubmitHandler (evt) {
   popupToggle ()
 }
 
+// Слушатели событий popup
 popupOverlay.addEventListener('click', popupToggle);
 editButton.addEventListener('click', popupToggle);
 closeButton.addEventListener('click', popupToggle);
