59d6a564c495c2ba51188404ea8faa829be7b89c
Iurii Bairamukov <juribai@mail.ru>
2020-07-01 18:35:08 +0700

    feat: сделать рефакторинг

--- scripts/index.js
+++ scripts/index.js
@@ -16,36 +16,34 @@ const popupPlacesForm = document.querySelector('.popup__form_add-places');
 const popupPlacesNameInput = document.querySelector('.popup__input_type_places-name');
 const popupPlacesImageInput = document.querySelector('.popup__input_type_place-image');
 
-// Слушатели событий popup для image
 const popupImage = document.querySelector('.popup_image-places');
-
 const popupImageCloseButton = document.querySelector('.popup__close-button_image-places');
 const popupImageOverlay = document.querySelector('.popup__overlay_image-places');
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Селигер',
+    link: '../images/seliger.jpg'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Сахалин',
+    link: '../images/sahalin_134.jpg'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Эвенкия',
+    link: '../images/plato-putarano.png'
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Якутия',
+    link: '../images/lensk-stolb.jpg'
   },
   {
     name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    link: '../images/baikal.jpg'
+  },
+  {
+    name: 'Красноярск',
+    link: '../images/kras-stolb.jpg'
   }
 ];
 
@@ -54,16 +52,8 @@ initialCards.forEach (item => {
   addPlaces (item.name, item.link);
 });
 
-
-
 // Функция переключатель класса у popup
 popupToggle = popup => {
-  if (!(popup.classList.contains('popup_opened'))) {
-    popupProfileNameInput.value = nameProfileUser.textContent;
-    popupProfileJobInput.value = jobProfileUser.textContent;
-    popupPlacesNameInput.value = '';
-    popupPlacesImageInput.value = '';
-  }
   popup.classList.toggle('popup_opened');
 }
 
@@ -79,80 +69,70 @@ formSubmitHandlerProfile = evt => {
 formSubmitHandlerPlaces = evt => {
   evt.preventDefault();
   addPlaces (popupPlacesNameInput.value, popupPlacesImageInput.value);
+  popupPlacesNameInput.value = '';
+  popupPlacesImageInput.value = '';
   popupToggle (popupPlaces);
 }
 
-// Функция добавления новых карточек
+// Функция добавления карточек
 function addPlaces (title, image) {
   const placesTemplate = document.querySelector('#places-template').content;
   const placesCardList = document.querySelector('.places__cards');
   const placesItems = placesTemplate.cloneNode(true);
   
-  const placesTitle = placesItems.querySelector('.places__title');
-  const placesImage = placesItems.querySelector('.places__image');
+  placesItems.querySelector('.places__title').textContent = title;
+  placesItems.querySelector('.places__image').src = image;
+  placesItems.querySelector('.places__image').alt = `${title}. Красивые места России.`;
   
-  placesTitle.textContent = title;
-  placesImage.src = image;
-  placesImage.alt = `${title}. Красивые места России.`;
-  
-  // Функция лайка places
-  const likePlacesbutton = placesItems.querySelector('.places__button-like');
-  likePlacesbutton.addEventListener('click', (evt) => {
-    const eventTarget = evt.target;
-    eventTarget.classList.toggle('places__button-like_enabled');
-  });
-  
-  // Функция удаления карточки
-  const deletePlacesButton = placesItems.querySelector('.places__button-delete');
-  deletePlacesButton.addEventListener('click', () => {
-    const placesItem = deletePlacesButton.closest('.places__items');
-    placesItem.remove();
-  });
+  likePlacesItem (placesItems);
+  deletePlacesItem (placesItems);
+  renderPopupImage (title, image, placesItems);
 
-  // Функция попапа карточки
-  const imageButton = placesItems.querySelector('.places__image');
-  const popupImagePlaces = document.querySelector('.popup__places-image');
-  const popupImageTitle = document.querySelector('.popup__places-title');
-  imageButton.addEventListener('click', () => {
-    popupImagePlaces.src = image;
-    popupImagePlaces.alt = `${title}. Красивые места России.`;
-    popupImageTitle.textContent = title;
-    popupToggle(popupImage)
-  });
-  
   placesCardList.prepend(placesItems);  
 }
 
+// Функция лайка places
+function likePlacesItem (cloneNode) {
+  cloneNode.querySelector('.places__button-like').addEventListener('click', (evt) => {
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle('places__button-like_enabled');
+  });
+}
+
+// Функция удаления карточки
+function deletePlacesItem (cloneNode) {
+  cloneNode.querySelector('.places__button-delete').addEventListener('click', () => {
+    const placesItem = deletePlacesButton.closest('.places__items');
+    placesItem.remove();
+  });
+};
+
+// Функция попапа Image
+function renderPopupImage (title, image, cloneNode) {
+  cloneNode.querySelector('.places__image').addEventListener('click', () => {
+    document.querySelector('.popup__places-image').src = image;
+    document.querySelector('.popup__places-image').alt = `${title}. Красивые места России.`;
+    document.querySelector('.popup__places-title').textContent = title;
+    popupToggle(popupImage);
+  });
+};
+
 // Слушатели событий popup для user
-popupProfileOverlay.addEventListener('click', () => {
-  popupToggle(popupProfile)
-});
 editProfileButton.addEventListener('click', () => {
+  popupProfileNameInput.value = nameProfileUser.textContent;
+  popupProfileJobInput.value = jobProfileUser.textContent;
   popupToggle(popupProfile)
 });
-popupProfileCloseButton.addEventListener('click', () => {
-  popupToggle(popupProfile)
-});
+popupProfileCloseButton.addEventListener('click', () => popupToggle(popupProfile));
+popupProfileOverlay.addEventListener('click', () => popupToggle(popupProfile));
 popupProfileForm.addEventListener('submit', formSubmitHandlerProfile);
 
-
 // Слушатели событий popup для places
-popupPlacesOverlay.addEventListener('click', () => {
-  popupToggle(popupPlaces)
-});
-addPlacesButton.addEventListener('click', () => {
-  popupToggle(popupPlaces)
-});
-popupPlacesCloseButton.addEventListener('click', () => {
-  popupToggle(popupPlaces)
-});
+addPlacesButton.addEventListener('click', () => popupToggle(popupPlaces));
+popupPlacesCloseButton.addEventListener('click', () => popupToggle(popupPlaces));
+popupPlacesOverlay.addEventListener('click', () => popupToggle(popupPlaces));
 popupPlacesForm.addEventListener('submit', formSubmitHandlerPlaces);
 
-
-popupImageOverlay.addEventListener('click', () => {
-  popupToggle(popupImage)
-});
-
-popupImageCloseButton.addEventListener('click', () => {
-  popupToggle(popupImage)
-});
+// Слушатели событий popup для popupImage
+popupImageOverlay.addEventListener('click', () => popupToggle(popupImage));
+popupImageCloseButton.addEventListener('click', () => popupToggle(popupImage));
