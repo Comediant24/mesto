2ee8ad1de919410f84d6f38ee06b5211259d1341
Iurii Bairamukov <juribai@mail.ru>
2020-07-01 14:12:43 +0700

    feat: добавить функция лайка

--- scripts/index.js
+++ scripts/index.js
@@ -17,74 +17,6 @@ const popupPlacesForm = document.querySelector('.popup__form_add-places');
 const popupPlacesNameInput = document.querySelector('.popup__input_type_places-name');
 const popupPlacesImageInput = document.querySelector('.popup__input_type_place-image');
 
-
-// Функция переключатель класса у popup
-popupToggle = popup => {
-  if (!(popup.classList.contains('popup_opened'))) {
-    popupProfileNameInput.value = nameProfileUser.textContent;
-    popupProfileJobInput.value = jobProfileUser.textContent;
-    popupPlacesNameInput.value = '';
-    popupPlacesImageInput.value = '';
-  }
-  popup.classList.toggle('popup_opened');
-}
-
-// Функция для отправки введенной информации profile
-formSubmitHandlerProfile = evt => {
-  evt.preventDefault();
-  nameProfileUser.textContent = popupProfileNameInput.value;
-  jobProfileUser.textContent = popupProfileJobInput.value;
-  popupToggle (popupProfile);
-}
-
-// Функция для отправки введенной информации places
-formSubmitHandlerPlaces = evt => {
-  evt.preventDefault();
-  addPlaces (popupPlacesNameInput.value, popupPlacesImageInput.value);
-  popupToggle (popupPlaces);
-}
-
-// Функция добавления новых карточек
-addPlaces = (title, image) => {
-  console.log(title, image)
-  const placesTemplate = document.querySelector('#places-template').content;
-  const placesCardList = document.querySelector('.places__cards');
-  const placesItems = placesTemplate.cloneNode(true);
-  
-  const placesTitle = placesItems.querySelector('.places__title');
-  const placesImage = placesItems.querySelector('.places__image');
-  
-  placesTitle.textContent = title;
-  placesImage.src = image;
-  
-  placesCardList.prepend(placesItems);
-}
-
-// Слушатели событий popup для user
-popupProfileOverlay.addEventListener('click', () => {
-  popupToggle(popupProfile)
-});
-editProfileButton.addEventListener('click', () => {
-  popupToggle(popupProfile)
-});
-popupProfileCloseButton.addEventListener('click', () => {
-  popupToggle(popupProfile)
-});
-popupProfileForm.addEventListener('submit', formSubmitHandlerProfile);
-
-
-// Слушатели событий popup для places
-popupPlacesOverlay.addEventListener('click', () => {
-  popupToggle(popupPlaces)
-});
-addPlacesButton.addEventListener('click', () => {
-  popupToggle(popupPlaces)
-});
-popupPlacesCloseButton.addEventListener('click', () => {
-  popupToggle(popupPlaces)
-});
-popupPlacesForm.addEventListener('submit', formSubmitHandlerPlaces);
-
 const initialCards = [
   {
       name: 'Архыз',
@@ -116,3 +48,79 @@ const initialCards = [
 initialCards.forEach (item => {
   addPlaces (item.name, item.link);
 });
+
+
+
+// Функция переключатель класса у popup
+popupToggle = popup => {
+  if (!(popup.classList.contains('popup_opened'))) {
+    popupProfileNameInput.value = nameProfileUser.textContent;
+    popupProfileJobInput.value = jobProfileUser.textContent;
+    popupPlacesNameInput.value = '';
+    popupPlacesImageInput.value = '';
+  }
+  popup.classList.toggle('popup_opened');
+}
+
+// Функция для отправки введенной информации profile
+formSubmitHandlerProfile = evt => {
+  evt.preventDefault();
+  nameProfileUser.textContent = popupProfileNameInput.value;
+  jobProfileUser.textContent = popupProfileJobInput.value;
+  popupToggle (popupProfile);
+}
+
+// Функция для отправки введенной информации places
+formSubmitHandlerPlaces = evt => {
+  evt.preventDefault();
+  addPlaces (popupPlacesNameInput.value, popupPlacesImageInput.value);
+  popupToggle (popupPlaces);
+}
+
+// Функция добавления новых карточек
+function addPlaces (title, image) {
+  console.log(title, image)
+  const placesTemplate = document.querySelector('#places-template').content;
+  const placesCardList = document.querySelector('.places__cards');
+  const placesItems = placesTemplate.cloneNode(true);
+  
+  const placesTitle = placesItems.querySelector('.places__title');
+  const placesImage = placesItems.querySelector('.places__image');
+  
+  placesTitle.textContent = title;
+  placesImage.src = image;
+
+  // Функция лайка places
+  const likePlacesbutton = placesItems.querySelector('.places__button-like');
+  likePlacesbutton.addEventListener('click', (evt) => {
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle('places__button-like_enabled');
+  });
+  
+  placesCardList.prepend(placesItems);  
+}
+
+// Слушатели событий popup для user
+popupProfileOverlay.addEventListener('click', () => {
+  popupToggle(popupProfile)
+});
+editProfileButton.addEventListener('click', () => {
+  popupToggle(popupProfile)
+});
+popupProfileCloseButton.addEventListener('click', () => {
+  popupToggle(popupProfile)
+});
+popupProfileForm.addEventListener('submit', formSubmitHandlerProfile);
+
+
+// Слушатели событий popup для places
+popupPlacesOverlay.addEventListener('click', () => {
+  popupToggle(popupPlaces)
+});
+addPlacesButton.addEventListener('click', () => {
+  popupToggle(popupPlaces)
+});
+popupPlacesCloseButton.addEventListener('click', () => {
+  popupToggle(popupPlaces)
+});
+popupPlacesForm.addEventListener('submit', formSubmitHandlerPlaces);
