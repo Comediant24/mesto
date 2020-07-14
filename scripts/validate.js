3e0ecdf4084030fbf3cdbcce4bdad72fbe4f198e
Iurii Bairamukov <juribai@mail.ru>
2020-07-14 13:03:15 +0700

    feat: добавить функцию валидации формы

---
+++ scripts/validate.js
@@ -0,0 +1,37 @@
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  // submitButtonSelector: '.popup__save-button',
+  // inactiveButtonClass: 'popup__save-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  // errorClass: 'popup__error_visible',
+});
+
+function enableValidation ( propertiesForm ) {
+  const formElements = Array.from(document.querySelectorAll(propertiesForm.formSelector));
+  
+  formElements.forEach( form => {
+    form.addEventListener('submit', evt => {
+      evt.preventDefault();
+    })
+    form.addEventListener('input', (evt) => {
+      isValid(evt, propertiesForm);
+    })
+  })
+}
+
+function isValid (evt, propertiesForm) {
+  if (!evt.target.validity.valid) {
+    showInputError(evt, propertiesForm, evt.target.validationMessage);
+  } else {
+    hideInputError(evt, propertiesForm);
+  }
+}
+
+function showInputError (evt, propertiesForm, errorMessage) {
+  evt.target.classList.add(propertiesForm.inputErrorClass);
+}
+
+function hideInputError (evt, propertiesForm) {
+  evt.target.classList.remove(propertiesForm.inputErrorClass);
+};
