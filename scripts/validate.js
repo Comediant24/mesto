b190c58b9226e1260a004f21506cf13a24a302da
Iurii Bairamukov <juribai@mail.ru>
2020-07-14 20:37:37 +0700

    feat: создать функцию disabled кнопки

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,35 +1,38 @@
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
-  // submitButtonSelector: '.popup__save-button',
-  // inactiveButtonClass: 'popup__save-button_disabled',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible',
 });
 
 function enableValidation ( propertiesForm ) {
   const formList = Array.from(document.querySelectorAll(propertiesForm.formSelector));
-  
-  formList.forEach( formElement => {
+
+  formList.forEach(formElement => {
+    
+    setEventListeners(formElement, propertiesForm);
     formElement.addEventListener('submit', evt => {
       evt.preventDefault();
     });
-
-    setEventListeners(formElement, propertiesForm);
   });
 }
 
 function setEventListeners (formElement, propertiesForm) {
-
+  
   const inputList = Array.from(formElement.querySelectorAll(propertiesForm.inputSelector));
+  const buttonElement = formElement.querySelector(propertiesForm.submitButtonSelector);
+
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement, propertiesForm)
+      isInputValid(inputElement, propertiesForm);
+      toggleButtonState(inputList, buttonElement, propertiesForm);
     });
   });
 }
 
-function isValid (input, propertiesForm) {
+function isInputValid (input, propertiesForm) {  
   if (!input.validity.valid) {
     showInputError(input, input.validationMessage, propertiesForm);
   } else {
@@ -47,4 +50,20 @@ function hideInputError (input, propertiesForm) {
   input.classList.remove(propertiesForm.inputErrorClass);
   input.nextElementSibling.textContent = '';
   input.nextElementSibling.classList.remove(propertiesForm.errorClass);
-};
+}
+
+function hasInvalidInput (inputList) {
+  return inputList.some(inputElement => {
+    return !inputElement.validity.valid;
+  })
+}
+
+function toggleButtonState (inputList, button, propertiesForm) {
+  if (hasInvalidInput(inputList)) {
+    button.classList.add(propertiesForm.inactiveButtonClass);
+    button.disabled = true;
+  } else {
+    button.classList.remove(propertiesForm.inactiveButtonClass);
+    button.disabled = false;
+  }
+}
