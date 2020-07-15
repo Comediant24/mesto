d9924328866bc3812db43116f6859b8c329282ad
Iurii Bairamukov <juribai@mail.ru>
2020-07-15 10:11:01 +0700

    feat: отрефакторить validate.js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,55 +1,22 @@
-function enableValidation ( propertiesForm ) {
-  const formList = Array.from(document.querySelectorAll(propertiesForm.formSelector));
-
-  formList.forEach(formElement => {
-    
-    setEventListeners(formElement, propertiesForm);
-    formElement.addEventListener('submit', evt => {
-      evt.preventDefault();
-    });
-  });
-}
-
-function setEventListeners (formElement, propertiesForm) {
-  
-  const inputList = Array.from(formElement.querySelectorAll(propertiesForm.inputSelector));
-  const buttonElement = formElement.querySelector(propertiesForm.submitButtonSelector);
-
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-      isInputValid(inputElement, propertiesForm);
-      toggleButtonState(inputList, buttonElement, propertiesForm);
-    });
-  });
-}
-
-function isInputValid (input, propertiesForm) {  
-  if (!input.validity.valid) {
-    showInputError(input, input.validationMessage, propertiesForm);
-  } else {
-    hideInputError(input, propertiesForm);
-  }
-}
-
-function showInputError (input, errorMessage, propertiesForm) {
+const showInputError = (input, errorMessage, propertiesForm) => {
   input.classList.add(propertiesForm.inputErrorClass);
   input.nextElementSibling.textContent = errorMessage;
   input.nextElementSibling.classList.add(propertiesForm.errorClass);
-}
+};
 
-function hideInputError (input, propertiesForm) {
+const hideInputError = (input, propertiesForm) => {
   input.classList.remove(propertiesForm.inputErrorClass);
   input.nextElementSibling.textContent = '';
   input.nextElementSibling.classList.remove(propertiesForm.errorClass);
-}
+};
 
-function hasInvalidInput (inputList) {
+const hasInvalidInput = inputList => {
   return inputList.some(inputElement => {
     return !inputElement.validity.valid;
   })
-}
+};
 
-function toggleButtonState (inputList, button, propertiesForm) {
+const toggleButtonState = (inputList, button, propertiesForm) => {
   if (hasInvalidInput(inputList)) {
     button.classList.add(propertiesForm.inactiveButtonClass);
     button.disabled = true;
@@ -57,4 +24,33 @@ function toggleButtonState (inputList, button, propertiesForm) {
     button.classList.remove(propertiesForm.inactiveButtonClass);
     button.disabled = false;
   }
-}
+};
+
+const isInputValid = (input, propertiesForm) => {  
+  if (!input.validity.valid) {
+    showInputError(input, input.validationMessage, propertiesForm);
+  } else {
+    hideInputError(input, propertiesForm);
+  }
+};
+
+const setEventListeners = (formElement, propertiesForm) => {
+  const inputList = Array.from(formElement.querySelectorAll(propertiesForm.inputSelector));
+  const buttonElement = formElement.querySelector(propertiesForm.submitButtonSelector);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isInputValid(inputElement, propertiesForm);
+      toggleButtonState(inputList, buttonElement, propertiesForm);
+    });
+  });
+};
+
+const enableValidation = propertiesForm => {
+  const formList = Array.from(document.querySelectorAll(propertiesForm.formSelector));
+  formList.forEach(formElement => {
+    setEventListeners(formElement, propertiesForm);
+    formElement.addEventListener('submit', evt => {
+      evt.preventDefault();
+    });
+  });
+};
