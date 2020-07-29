01ad19310940b77b762632deb7259937f0cd6e00
Iurii Bairamukov <juribai@mail.ru>
2020-07-29 08:05:04 +0700

    feat: создать initialCards.js

---
+++ scripts/initialCards.js
@@ -0,0 +1,26 @@
+export const initialCards = [
+  {
+    name: 'Селигер',
+    link: './images/seliger.jpg'
+  },
+  {
+    name: 'Сахалин',
+    link: './images/sahalin_134.jpg'
+  },
+  {
+    name: 'Эвенкия',
+    link: './images/plato-putarano.png'
+  },
+  {
+    name: 'Якутия',
+    link: './images/lensk-stolb.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: './images/baikal.jpg'
+  },
+  {
+    name: 'Красноярск',
+    link: './images/kras-stolb.jpg'
+  }
+];
