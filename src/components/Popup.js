d8c8d93d49c5eeed332d891d1b5292387d97358b
Iurii Bairamukov <juribai@mail.ru>
2020-08-07 06:16:54 +0700

    feat: создать класс Popup

---
+++ src/components/Popup.js
@@ -0,0 +1,29 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    this._handleEscClose();
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+  }
+
+  _handleEscClose() {
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape' && this._popup.classList.contains('popup_opened')) {
+        this.close();
+      }
+    });
+  }
+
+  setEventListeners() {
+    this._popup.querySelector('.popup__close-button')
+      .addEventListener('click', () => this.close());
+    this._popup.querySelector('.popup__overlay')
+      .addEventListener('click', () => this.close());
+  }
+}
